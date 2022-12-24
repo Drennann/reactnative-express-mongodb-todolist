b11fff1844bbdb01daf5680c9a898362eb7a96e8
Drennann <delpan.drennan@gmail.com>
2022-12-23 21:16:25 -0300

    Added tryCatch blocks in API requests

--- app/api.js
+++ app/api.js
@@ -1,45 +1,65 @@
 const API = "http://10.0.2.2:3001/api/tasks"
 
 export const getTasks= async () => {
-    const response = await fetch(API);
-    const tasks = await response.json();
-    return tasks;
+    try{
+        const response = await fetch(API);
+        const tasks = await response.json();
+        return tasks;
+    }catch(error){
+        console.log(error)
+    }
 }
 
 export const postTask= async (body) => {
-    const response = await fetch(API, {
-        headers:{
-            "Content-Type": "Application/json"
-        },
-        method: "POST",
-        body: JSON.stringify(body)
-    });
-    const status = await response.json();
-    return status;
+    try{
+        const response = await fetch(API, {
+            headers:{
+                "Content-Type": "Application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(body)
+        });
+        const status = await response.json();
+        return status;
+    }catch(error){
+        console.log(error)
+    }
 }
 
 export const putTask= async (id, body) => {
-    const response = await fetch(API+"/"+id, {
-        headers:{
-            "Content-Type": "Application/json"
-        },
-        method: "PUT",
-        body: JSON.stringify(body)
-    });
-    const status = await response.json();
-    return status;
+    try{
+        const response = await fetch(API+"/"+id, {
+            headers:{
+                "Content-Type": "Application/json"
+            },
+            method: "PUT",
+            body: JSON.stringify(body)
+        });
+        const status = await response.json();
+        return status;
+    }catch(error){
+        console.log(error)
+    }
 }
 
 export const deleteTask= async (id) => {
-    const response = await fetch(API+"/"+id,{
-        method: "DELETE"
-    });
-    const status = await response.json();
-    return status;
+    try{
+        const response = await fetch(API+"/"+id,{
+            method: "DELETE"
+        });
+        const status = await response.json();
+        return status;
+    }catch(error){
+        console.log(error)
+    }
 }
 
 export const getTask= async (id) => {
-    const response = await fetch(API+"/"+id);
-    const task = await response.json();
-    return task;
+    try{
+        const response = await fetch(API+"/"+id);
+        const task = await response.json();
+        return task;
+    }catch(error){
+        console.log(error)
+    }
 }
